326a315ddcea0df753072df3edf008118bf46a0b
“cuikuikui” <990417665@qq.com>
2021-07-18 21:35:08 +0800

    main.js 中配置 element 组件库

--- src/main.js
+++ src/main.js
@@ -4,10 +4,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载 element 组件库
+import ElementUI from 'element-ui'
+
+// 加载 element 组件库的样式
+import 'element-ui/lib/theme-chalk/index.css'
+
 // 加载全局样式文件
 import './styles/index.less'
 
 Vue.config.productionTip = false
+
+// 全局注册 element 组件库
+Vue.use(ElementUI)
 
 // 创建Vue的根实例
 // 把router配置到根实例中
